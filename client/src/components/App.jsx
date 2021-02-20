784e1732549bc86ccc30551e593d983a1052a016
ChrisHolley <asiholley@gmail.com>
2021-02-20 10:39:15 -0800

    added map to App to display local movie titles
    Next:
    o add movie component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
 const App = (props) => (
-  <div>Hello World!</div>
+  <div>
+    <ul>
+    {movies[0].title}
+    {movies.map((movie, idx) => (
+      <li>
+        {movie.title}
+      </li>
+    ))}
+    </ul>
+  </div>
 );
 
 export default App;
